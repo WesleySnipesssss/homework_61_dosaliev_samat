d05be5ba99f06bc10e942894d607c545901eab69
Samat <0704778866s@gmail.com>
2024-12-19 12:42:20 +0600

    Добавлено поле project в модель задачи

--- tracker/models.py
+++ tracker/models.py
@@ -22,6 +22,15 @@ class Type(models.Model):
     def __str__(self):
         return self.name
 
+class Project(models.Model):
+    start_date = models.DateField(verbose_name="Дата начала")
+    end_date = models.DateField(verbose_name="Дата окончания", null=True, blank=True)
+    name = models.CharField(max_length=255, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание", blank=True)
+
+    def __str__(self):
+        return self.name
+
 class Issue(models.Model):
     summary = models.CharField(max_length=100, validators=[validate_summary_length])
     description = models.TextField(blank=True, null=True)
@@ -30,6 +39,14 @@ class Issue(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     due_date = models.DateField(validators=[validate_due_date], blank=True, null=True)
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Проект"
+    )
 
     def __str__(self):
         return self.summary
+
